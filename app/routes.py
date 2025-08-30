4791825d6a419b08b91a13a874223ee0ead7954c
icaro <icaroxwagner777@gmail.com>
2025-08-30 19:41:21 -0300

    inserindo dados na tabela

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,32 @@
-from app import app
-from flask import render_template, url_for #renderizar arquivo html
+from app import app, db
+from flask import render_template, url_for, request #renderizar arquivo html
+from app.models import Contato
 
 @app.route("/")
 def homepage():
     return render_template("index.html")
 
-@app.route("/roupa/")
+@app.route("/contato/", methods = ["GET", "POST"])
 def secondpage():
-    return "Essa é a nova rota"
+   context = {}
+   if request.method == "GET":
+      pesquisa = request.args.get("pesquisa")
+      context.update({"pesquisa" : pesquisa})
+
+   if request.method == "POST":
+      nome = request.form["nome"]
+      email = request.form["email"]
+      assunto = request.form["assunto"]
+      mensagem = request.form["mensagem"]
+
+      contato = Contato(
+         nome = nome,
+         email = email,
+         assunto = assunto,
+         mensagem = mensagem
+      )
+
+      db.session.add(contato)
+      db.session.commit()
+
+   return render_template("contato.html", context = context)
